5bfc07ed6a82e323b522b5d2db4c3ca0181b6ba9
TigranBalayan10 <tigranbalayan10@gmail.com>
2023-07-07 22:57:44 -0700

    failed paymentIntent added as a case in nbackend

--- server/config/webhooks.js
+++ server/config/webhooks.js
@@ -1,12 +1,31 @@
 const { Order } = require("../models"); // Import the Order model or your database schema
 
 const handleWebhookEvent = async (event) => {
+  let orderId;
+  let paymentStatus;
   switch (event.type) {
     case "payment_intent.succeeded":
       const paymentIntentSucceeded = event.data.object;
-      const orderId = paymentIntentSucceeded.metadata.orderId;
-      const paymentStatus = "Paid" // Set the desired payment status
+      orderId = paymentIntentSucceeded.metadata.orderId;
+      paymentStatus = "Paid"; // Set the desired payment status
 
+      try {
+        const order = await Order.findOneAndUpdate(
+          { _id: orderId },
+          { paymentStatus: paymentStatus },
+          { new: true }
+        );
+        console.log("Order updated:", order);
+      } catch (error) {
+        console.log("Error updating order:", error);
+      }
+      break;
+    // ... handle other event types
+    case "payment_intent.payment_failed":
+      const paymentIntentPaymentFailed = event.data.object;
+      // Then define and call a function to handle the event payment_intent.payment_failed
+      orderId = paymentIntentPaymentFailed.metadata.orderId;
+      paymentStatus = "Failed"; // Set the desired payment status
       try {
         const order = await Order.findOneAndUpdate(
           { _id: orderId },
