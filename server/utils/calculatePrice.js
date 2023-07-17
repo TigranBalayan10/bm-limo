11c70c37598ec66fd4f3a1f7b8e16f7916505e65
TigranBalayan10 <tigranbalayan10@gmail.com>
2023-07-16 22:24:48 -0700

    stripe getting flat price for checkout in backend

--- server/utils/calculatePrice.js
+++ server/utils/calculatePrice.js
@@ -1,7 +1,12 @@
 const calculatePrice = (order) => {
   const { price } = order;
   const { priceTotal } = price;
+  const { flatRate } = price;
+  const { flatPrice } = flatRate;
   const { hourly, mileage } = priceTotal;
+  if (flatPrice > 0) {
+    return flatPrice;
+  }
   const total = hourly > 0 ? hourly : mileage;
   return total;
 };
